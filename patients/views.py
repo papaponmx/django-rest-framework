cb779048935f3a57cd2974228001b86854d62916
Jaime Rios <jaime.rios@hey.com>
2024-10-10 15:16:50 -0600

    Add patient detail view and refactor patient listing endpoint; update URL routing

--- patients/views.py
+++ patients/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-
+# Since this does not take parameters we will use the same view
 @api_view(["GET", "POST"])
 def list_patients(request):
     if request.method == 'GET':
@@ -14,7 +14,18 @@ def list_patients(request):
         return Response(serializer.data)
     if request.method == 'POST':
         serializer = PatientSerializer(data=request.data)  # Deserializar los datos enviados por el cliente
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()  # Guardar el nuevo paciente en la base de datos
-            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Devolver los datos del paciente creado
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Si hay errores, devolverlos
+        serializer.is_valid(raise_exception=True)
+        serializer.save()  # Guardar el nuevo paciente en la base de datos
+        return Response(serializer.data, status=status.HTTP_201_CREATED)  # Devolver los datos del paciente creado
+
+@api_view(["GET", "PUT", "DELETE"])
+def detail_patient(request, pk):
+    if request.method == 'GET':
+        try: 
+            patient = Patient.objects.get(id=pk)
+        except Patient.DoesNotExist:
+            return Response({"error": "Patient not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
