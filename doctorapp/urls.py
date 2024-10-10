6c089acb67c23ff9c26bea7297c463e4a8bf638f
Jaime Rios <jaime.rios@hey.com>
2024-10-10 14:53:08 -0600

    Add patient listing and creation endpoints; update .gitignore for __pycache__

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from patients.views import list_patients
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/patients', list_patients),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .serializers import PatientSerializer
+from .models import Patient
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def list_patients(request):
+    patients = Patient.objects.all()
+    serializer = PatientSerializer(patients, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def create_patient(request):
+    serializer = PatientSerializer(data=request.data)  # Deserializar los datos enviados por el cliente
+    if serializer.is_valid():  # Verificar si los datos son válidos
+        serializer.save()  # Guardar el nuevo paciente en la base de datos
+        return Response(serializer.data, status=status.HTTP_201_CREATED)  # Devolver los datos del paciente creado
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Si hay errores, devolverlos
