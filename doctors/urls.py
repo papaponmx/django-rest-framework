b08709eb6d4126abc9368291ca23e96f058c4528
Jaime Rios <jaime.rios@hey.com>
2024-10-13 16:48:20 -0600

    Add DoctorViewSet and update URL routing for doctors API

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 
 from doctors.views import ListDoctorsViewSet, DetailDoctorView 
 
+from rest_framework.routers import DefaultRouter
+from .viewsets import DoctorViewSet
+
+router = DefaultRouter()
+router.register('doctors', DoctorViewSet, basename='doctor')
+
 urlpatterns = [
     path('doctors', ListDoctorsViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('doctors/<int:pk>/', DetailDoctorView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-]
+] + router.urls
---
+++ doctors/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+
+from .serializers import DoctorSerializer
+from .models import Doctor
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
