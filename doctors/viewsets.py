0e6c4c64b98f48699e2ee83f0cd07b440d7792c3
Jaime Rios <jaime.rios@hey.com>
2024-10-13 17:17:48 -0600

    Add vacation management actions to DoctorViewSet

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabiltySerializer, MedicalNoteSerializer
 from .models import Doctor, Department, DoctorAvailabilty, MedicalNote
@@ -7,6 +9,20 @@ class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
     
+    @action(["POST"], detail=True, url_path="set-on-vacation")
+    def set_on_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({"is_on_vacation": doctor.is_on_vacation})
+
+    @action(["POST"], detail=True, url_path="set-off-vacation")
+    def set_off_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({"is_on_vacation": doctor.is_on_vacation})
+
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
