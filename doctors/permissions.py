918e8c8e84e3f0e347ff8a8023c347166165f496
Jaime Rios <jaime.rios@hey.com>
2024-10-14 18:39:55 -0600

    Update email validation in DoctorSerializer to allow @hey.com domain and add contact number validation for vacation status

--- doctors/permissions.py
+++ doctors/permissions.py
@@ -2,4 +2,4 @@ from rest_framework import permissions
 
 class IsDoctor(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name="Doctor").exists()
+        return request.user.groups.filter(name="doctors").exists()
--- doctors/serializers.py
+++ doctors/serializers.py
@@ -7,10 +7,17 @@ class DoctorSerializer(serializers.ModelSerializer):
         model = Doctor
         fields = '__all__'
         
-        def validate_email(self, value):
-            if "@example.com" in value:
-                return value
-            raise serializers.ValidationError("Email has to be example.com")
+    def validate_email(self, value):
+        if "@example.com"  in value or "@hey.com" in value:
+            return value
+        raise serializers.ValidationError("Email has to be example.com")
+        
+    def validate(self, attrs):
+        if len(attrs['contact_number']) < 10 and attrs['is_on_vacation']:
+            raise serializers.ValidationError(
+                "Contact number cannot be more than 10 digits when on vacation"
+            )
+        return super().validate(attrs)
             
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
