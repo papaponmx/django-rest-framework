0cbb6de26b41c549b89cc7b75054223a4cd23cd5
Jaime Rios <jaime.rios@hey.com>
2024-10-16 11:38:53 -0600

    Add appointments management to DoctorViewSet with POST and GET actions

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from bookings.serializers import AppointmentSerializer
+from bookings.models import Appointment
 from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabiltySerializer, MedicalNoteSerializer
 from .models import Doctor, Department, DoctorAvailabilty, MedicalNote
 from .permissions import IsDoctor
@@ -25,6 +27,37 @@ class DoctorViewSet(viewsets.ModelViewSet):
         doctor.is_on_vacation = False
         doctor.save()
         return Response({"is_on_vacation": doctor.is_on_vacation})
+    
+    @action(['POST', 'GET'], detail=True, serializer_class=AppointmentSerializer)
+    def appointments(self,  request, pk=None):
+        doctor =  self.get_object()
+        
+        if request.method  == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id            
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data,  status=status.HTTP_201_CREATED)
+        
+        if request.method == 'GET':
+            appointments = Appointment.objects.filter(doctor=doctor)
+            serializer = AppointmentSerializer(appointments, many=True)
+            return Response(serializer.data)
+    
+    @action(['GET', 'DELETE'], detail=True, url_path='appointments/(?P<appointment_id>[0-9]+)')
+    def appointment(self, request, pk=None, appointment_id=None):
+        
+        if request.method == 'GET':
+            appointments = Appointment.objects.get(id=appointment_id)
+            serializer = AppointmentSerializer(appointments)
+            return Response(serializer.data)
+        if request.method == 'DELETE':
+            doctor = self.get_object()
+            appointment = Appointment.objects.get(id=appointment_id)
+            appointment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
